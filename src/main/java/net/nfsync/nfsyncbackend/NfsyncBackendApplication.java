0c4c635c68322b3cab34299bd619cd22495a07b5
Maxim Puzanov <maxizenit@gmail.com>
2023-11-25 15:48:47 +0300

    Initial commit

---
+++ src/main/java/net/nfsync/nfsyncbackend/NfsyncBackendApplication.java
@@ -0,0 +1,13 @@
+package net.nfsync.nfsyncbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfsyncBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfsyncBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/nfsync/nfsyncbackend/NfsyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nfsync.nfsyncbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfsyncBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
